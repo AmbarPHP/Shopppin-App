22b153aa3483bb2178739e6c9eb45089992909c0
Marina <2175567+AmbarPHP@users.noreply.github.com>
2022-08-12 13:44:07 -0600

    Add comercial to the store

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,16 +1,20 @@
-import Container from "react-bootstrap/Container";
+
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import {Link} from "react-router-dom";
 import { Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import yourImg from "../assets/shooping.svg";
 import logo from "../assets/logo.png";
+import headphones from "../assets/blue-headphones.jpg";
 import "./NavBar.scss";
 import avatar from "../assets/avatar.jpg";
 
 function NavBar() {
   return (
+    <>
+    <Row class="gap-y align-items-center" style={{backgroundColor:'rgba(0, 157, 206)'}}>
     <Navbar sticky="top" className="bg-white shadow-sm mb-3" expand="md">
       <Container>
         <Navbar.Brand>
@@ -48,6 +52,21 @@ function NavBar() {
         
       </Container>
     </Navbar>
+      <Col sm={12} md={6} lg={6} className="px-0 order-md-2">
+        <img className="ms-auto" style={{backgroundColor:'blue', width:620, height:620}} 
+        src={headphones} alt="..."/>
+      </Col>
+      <Col sm={12} md={6} lg={4} className="ms-lg-auto">
+            <div class="text-center text-lg-start text-lg-nowrap">
+              <h4 class="text-light font-weight-light mb-0 pb-1">What you were waiting for?</h4>
+              <h1 class="text-contrast bold display-4">The New Headphones Collection</h1>
+              <p class="lead text-light pb-3">Discover our selection of the best Headphones</p>
+            </div>
+      </Col>
+    </Row>
+
+    
+    </>
   );
 }
 
